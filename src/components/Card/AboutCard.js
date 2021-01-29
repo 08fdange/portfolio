5ac2719b50f36849edb2da385d3a51d31bda1e17
08fdange <08fdange@gmail.com>
2021-01-28 21:24:53 -0500

    Centered About Me div

--- src/components/Card/AboutCard.js
+++ src/components/Card/AboutCard.js
@@ -5,8 +5,8 @@ const AboutCard = () => {
     return(
         <div className="card">
             <div className="details">
-                <p className="location">
-                    <strong>About me</strong><br/>
+                <div align='center'><h2>About Me</h2></div>
+                <p>
                     Full-Stack developer with experience in Ruby and JavaScript based programming, with knowledge of multiple frameworks and a former member of the Steelworkers’ Union. 
                     Possess strong skills in problem-solving and time management that helps meet deadlines while delivering quality end-results. 
                     Committed to continually learning and creating aesthetic interfaces, always keeping the user in mind. 
